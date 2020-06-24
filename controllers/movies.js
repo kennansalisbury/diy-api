0cbf557da29c8907ab94e4a53fbe5a85c8c3564f
kennansalisbury <kennansalisbury@gmail.com>
2020-06-24 10:54:12 -0400

    crud routes in place and working with database

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,28 +1,106 @@
 const router = require('express').Router()
+const db = require('../models')
 
 //GET - index
 router.get('/', (req, res) => {
-    res.send('send all movies')
+
+    db.movie.findAll()
+    .then(movies => {
+        res.send(movies)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).send('internal server error')
+    })
+
+    // res.send('send all movies')
 })
 
 //POST - add movie
 router.post('/', (req, res) => {
-    res.send('add movie')
+ 
+    db.movie.findOrCreate({
+        where: {
+            title: req.body.title
+        },
+        defaults: {
+            year: req.body.year,
+            famous_line: req.body.famous_line
+        }
+    })
+    .then(movie => {
+        res.send(movie)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).send('internal server error')
+    })
+    // res.send('add movie')
+
 })
 
 //GET - /movies/:id - show 1 movie
 router.get('/:id', (req, res) => {
-    res.send('show 1 movie')
+    
+    db.movie.findOne({
+        id: req.params.id
+    })
+    .then(movie => {
+        res.send(movie)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).send('internal server error')
+    })
+    
+    // res.send('show 1 movie')
 })
 
 //PUT - /movies/:id - update 1 movie
 router.put('/:id', (req, res) => {
-    res.send('update 1 movie')
+
+    db.movie.update(
+        req.body, 
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(updated => {
+        db.movie.findByPk(req.params.id)
+        .then(movie => {
+            res.send(movie)
+        })
+        .catch(err => {
+            console.log('error finding updated movie', err)
+            res.status(500).send('internal server error, error ')
+        })
+    })
+    .catch(err => {
+        console.log('error updating movie', err)
+        res.status(500).send('internal server error')
+    })
+    
+    // res.send('update 1 movie')
 })
 
 //DELETE - /movies/:id - delete 1 movie
 router.delete('/:id', (req, res) => {
-    res.send('delete 1 movie')
+    
+    db.movie.destroy({
+        where: {id: req.params.id}
+    })
+    .then(deleted => {
+        console.log(deleted)
+        res.send('movie deleted')
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).send('internal server error')
+    })
+    
+    // res.send('delete 1 movie')
 })
 
 module.exports = router
